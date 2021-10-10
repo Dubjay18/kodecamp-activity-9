cef12d5d42939c5022654dde87c4f0dc1b3151a9
Dub_jay <jejeniyi7@gmail.com>
2021-10-10 03:34:52 -1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+let image = [
+{
+    name: 'Car 1',
+    image_src: 'car-1',
+    Price: '$14.9 Million'
+},
+{
+    name: 'Car 2',
+    image_src: 'car-2',
+    Price: '$30 Million'
+},
+{
+    name: 'Car 3',
+    image_src: 'car-3',
+    Price: '$12 Million'
+},
+{
+    name: 'Car 4',
+    image_src: 'car-4',
+    Price: '$24 Million'
+},
+{
+    name: 'Car 5',
+    image_src: 'car-5',
+    Price: '$2 Million'
+},
+{
+    name: 'Car 6',
+    image_src: 'car-6',
+    Price: '$40 Million'
+},
+{
+    name: 'Car 7',
+    image_src: 'car-7',
+    Price: '$29 Million'
+},
+{
+    name: 'Car 8',
+    image_src: 'car-8',
+    Price: '$13 Million'
+},
+{
+    name: 'Car 9',
+    image_src: 'car-9',
+    Price: '$12.8 Million'
+},
+{
+    name: 'Car 10',
+    image_src: 'car-10',
+    Price: '$27 Million'
+},
+{
+    name: 'Car 11',
+    image_src: 'car-11',
+    Price: '$10 Million'
+},
+{
+    name: 'Car 12',
+    image_src: 'car-12',
+    Price: '$20.5 Million'
+}
+]
+let gallerysection = document.getElementById('wrapper')
+let html ="";
+image.forEach(e => {
+    html += `
+    <div class="img-card border-5 my-5 col-5 border-dark w-25 bg-light text-center mx-2 h-100  d-flex flex-wrap rounded-3 shadow-lg">
+    <div class="img-image w-auto">
+        <img src="images/${e.image_src}.jpg" alt="Car 1 />
+    </div>
+    <div class="image-info ">
+        <p>Name: ${e.name}<br />
+        Price: ${e.Price}</p>
+        
+    </div>
+</div>`
+
+console.log(e)
+
+gallerysection.innerHTML = html
+})
